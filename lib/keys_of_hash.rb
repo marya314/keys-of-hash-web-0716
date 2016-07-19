0fd8e85dc41e6a6f93332cd8fd1ba7003e5cc7b7
Mary Boyd <mary.boyd314@gmail.com>
2016-07-19 19:23:56 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,17 @@
 class Hash
-  def keys_of(arguments)
-    # code goes here
-  end
+  def keys_of(*arguments)
+    new_array = []
+    arguments.each do |arg|
+      self.each do |key, value|
+        if arg == value
+          new_array << key
+        end
 end
+end
+new_array
+end
+
+end
+
+
+#why use self? what does that do?
